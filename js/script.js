ffc5ef7e7a1fcff68560a1eab3b025a651fe3e7e
Ife Jeremiah <jerryroboy.16@gmail.com>
2021-10-06 00:47:06 +0100

    Updated the locations and zoom

--- js/script.js
+++ js/script.js
@@ -1,15 +1,15 @@
 // Initialize and add the map
 function initMap() {
   // The location of Uluru
-  const uluru = { lat: 6.489810, lng: 3.356230 };
-  // The map, centered at Uluru
+  const location = { lat: 6.489810, lng: 3.356230 };
+  // The map, centered at location
   const map = new google.maps.Map(document.getElementById("map"), {
-    zoom: 18,
-    center: uluru,
+    zoom: 16,
+    center: location,
   });
-  // The marker, positioned at Uluru
+  // The marker, positioned at location
   const marker = new google.maps.Marker({
-    position: uluru,
+    position: location,
     map: map,
   });
 }
